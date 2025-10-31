ad7636e64a4fb75e5f047e47bc832aa5bea7a6e4
Jess437 <b102513933@gmail.com>
2025-10-31 14:58:51 +0800

    chore: update the pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -4,7 +4,7 @@ export CUDA_VISIBLE_DEVICES=3
 export dataset_id=100
 export fold_id=0                        # (0, 1, 2, 3, 4)
 export configuration=3d_fullres         # (2d, 3d_fullres, 3d_lowres, 3d_cascade_fullres)
-export trainer=nnUNetTrainer_100epochs  # refer to ./nnunetv2/training/nnUNetTrainer/variants/training_length/nnUNetTrainer_Xepochs.py to set desired epochs
+export trainer=nnUNetTrainer            # refer to ./nnunetv2/training/nnUNetTrainer/variants/training_length/nnUNetTrainer_Xepochs.py to set desired epochs
 export plan=nnUNetPlans
 
 # if the preprocessed dataset for Dataset100_Heart does not exist, run preprocessing
@@ -38,7 +38,7 @@ nnUNetv2_find_best_configuration $dataset_id -c $configuration -f $fold_id -tr $
 
 # then go to ./nnUNet_results/Dataset100_Heart/inference_instructions.txt fetch the best inference commands (better)
 # or you can just simply use below two commands
-# "nnUNetv2_predict -d $dataset_id -c $configuration -f  $fold_id -tr $trainer -i ./nnUNet_raw/Dataset100_Heart/imagesTs -o ./nnUNet_raw/Dataset100_Heart/predict"
+# "nnUNetv2_predict -d $dataset_id -i ./nnUNet_raw/Dataset100_Heart/imagesTs -o ./nnUNet_raw/Dataset100_Heart/predict -f $fold_id -tr $trainer -c $configuration -p $plan"
 # "nnUNetv2_apply_postprocessing -i ./nnUNet_raw/Dataset100_Heart/predict -o ./nnUNet_raw/Dataset100_Heart/predict_pp -pp_pkl_file ./nnUNet_results/Dataset100_Heart/${trainer}__${plan}__${configuration}/crossval_results_folds_${fold_id}/postprocessing.pkl -np 8 -plans_json ./nnUNet_results/Dataset100_Heart/${trainer}__${plan}__${configuration}/crossval_results_folds_${fold_id}/plans.json"
 
 # then use 
