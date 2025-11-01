7b8495af8e75ab43fc5510e0cd8908f2d68e9647
Jess437 <b102513933@gmail.com>
2025-11-01 20:01:09 +0800

    feat: add ensemble_pipeline.sh for automated nnUNet ensemble workflow

---
+++ ensemble_pipeline.sh
@@ -0,0 +1,54 @@
+set -e
+
+export CUDA_VISIBLE_DEVICES=3
+export dataset_id=100
+export configuration=3d_fullres         # (2d, 3d_fullres, 3d_lowres, 3d_cascade_fullres)
+export trainer=nnUNetTrainer            # refer to ./nnunetv2/training/nnUNetTrainer/variants/training_length/nnUNetTrainer_Xepochs.py to set desired epochs
+export plan=nnUNetPlans
+
+# if the preprocessed dataset for Dataset100_Heart does not exist, run preprocessing
+if [ ! -d "./nnUNet_preprocessed/Dataset100_Heart" ]; then
+    nnUNetv2_plan_and_preprocess -d $dataset_id --verify_dataset_integrity
+fi
+
+# print training start message
+echo "=================================================================================="
+echo "Training started"
+echo "=================================================================================="
+
+# training (note: validation in this step is only partial to save time)
+nnUNetv2_train $dataset_id $configuration 0 -tr $trainer -p $plan
+nnUNetv2_train $dataset_id $configuration 1 -tr $trainer -p $plan
+nnUNetv2_train $dataset_id $configuration 2 -tr $trainer -p $plan
+nnUNetv2_train $dataset_id $configuration 3 -tr $trainer -p $plan
+nnUNetv2_train $dataset_id $configuration 4 -tr $trainer -p $plan
+
+# print validation start message
+echo "=================================================================================="
+echo "Validation started"
+echo "=================================================================================="
+
+# complete validation
+nnUNetv2_train $dataset_id $configuration 0 -tr $trainer -p $plan --val --val_best --npz
+nnUNetv2_train $dataset_id $configuration 1 -tr $trainer -p $plan --val --val_best --npz
+nnUNetv2_train $dataset_id $configuration 2 -tr $trainer -p $plan --val --val_best --npz
+nnUNetv2_train $dataset_id $configuration 3 -tr $trainer -p $plan --val --val_best --npz
+nnUNetv2_train $dataset_id $configuration 4 -tr $trainer -p $plan --val --val_best --npz
+
+# print best inference command message
+echo "=================================================================================="
+echo "Best inference command generation started"
+echo "=================================================================================="
+
+# to generate best inference command
+nnUNetv2_find_best_configuration $dataset_id -c $configuration -f 0 1 2 3 4 -tr $trainer -p $plan
+
+# then go to ./nnUNet_results/Dataset100_Heart/inference_instructions.txt fetch the best inference commands (better)
+# or you can just simply use below two commands
+nnUNetv2_predict -d $dataset_id -i ./nnUNet_raw/Dataset100_Heart/imagesTs -o ./nnUNet_raw/Dataset100_Heart/predict -f 0 1 2 3 4 -tr $trainer -c $configuration -p $plan --save_probabilities
+nnUNetv2_ensemble -i ./nnUNet_raw/Dataset100_Heart/predict -o ./nnUNet_raw/Dataset100_Heart/predict_ensemble -np 8
+nnUNetv2_apply_postprocessing -i ./nnUNet_raw/Dataset100_Heart/predict_ensemble -o ./nnUNet_raw/Dataset100_Heart/predict_pp -pp_pkl_file ./nnUNet_results/Dataset100_Heart/${trainer}__${plan}__${configuration}/crossval_results_folds_0_1_2_3_4/postprocessing.pkl -np 8 -plans_json ./nnUNet_results/Dataset100_Heart/${trainer}__${plan}__${configuration}/crossval_results_folds_0_1_2_3_4/plans.json
+
+# then use
+python rename_data.py
+zip -r ./predict.zip ./nnUNet_raw/Dataset100_Heart/predict_pp
